761255194372dee6cb00717bd955552f75046b14
zch <CHONG589@outlook.com>
2025-01-12 12:01:28 +0800

    socket_1

---
+++ code/macro.h
@@ -0,0 +1,16 @@
+/**
+ * @file macro.h
+ * @brief 常用宏的封装
+ * @author zch
+ * @date 2025-01-12
+ */
+
+#ifndef __ZCH_MACRO_H__
+#define __ZCH_MACRO_H__
+
+#include <string.h>
+#include <assert.h>
+#include "./log/log.h"
+#include "util.h"
+
+#endif
---
+++ code/socket.cpp
@@ -0,0 +1,8 @@
+#include <limits.h>
+
+#include "socket.h"
+#include "iomanager.h"
+#include "fd_manager.h"
+#include "./log/log.h"
+#include "macro.h"
+#include "hook.h"
